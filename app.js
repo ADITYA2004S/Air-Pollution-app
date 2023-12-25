188e382fe1bcf1ea229a2d8a47c3e3da939899a6
Aditya <adityasinghofficial06@gmail.com>
2023-12-25 20:28:46 +0530

    Added footer

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.render("navbar.ejs");
 });
 
+app.get("/footer", (req, res) => {
+  res.render("footer.ejs");
+});
+
 app.listen(3000, () => {
   console.log("server is active at 3000");
 });
