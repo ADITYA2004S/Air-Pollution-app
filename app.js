cf14076f41c03f2a6104e545d5fe50c8eedfc136
Aditya <adityasinghofficial06@gmail.com>
2023-12-27 01:33:41 +0530

    Added main-body

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get("/", (req, res) => {
-//   res.send("hello");
-// });
-
 app.get("/", (req, res) => {
   res.render("navbar.ejs");
 });
@@ -18,6 +14,11 @@ app.get("/", (req, res) => {
 app.get("/footer", (req, res) => {
   res.render("footer.ejs");
 });
+app.get("/body", (req, res) => {
+  res.render("main-body.ejs");
+});
+
+// ------------------------------API Connection-------------------------------------------//
 
 app.post("/result", (req, res) => {
   var city = req.body.city;
@@ -31,13 +32,23 @@ app.post("/result", (req, res) => {
     function (error, response, body) {
       var data = JSON.parse(body);
       var aqi = data.overall_aqi;
+      var NO2 = data.NO2.concentration;
+      var PM10 = data.PM10.concentration;
+      var SO2 = data.SO2.concentration;
       console.log(aqi);
+      console.log(NO2);
+      console.log(PM10);
+      console.log(SO2);
       res.write("<h1>" + city + " " + "</h1>");
+      res.write("<p> NO2 concentration is " + NO2 + "</p>");
+      res.write("<p> PM10 concentration is " + PM10 + "</p>");
+      res.write("<p> SO2 concentration is " + SO2 + "</p>");
       res.write("<p> AQI is " + aqi + "</p>");
       res.send();
     }
   );
 });
+//-----------------------------------------------------------------------------------------//
 
 app.listen(3000, () => {
   console.log("server is active at 3000");
